3c54828e8fb11795d5721a4b218caece4ba9dcb4
Dexshine <b_o_y_s@hotmail.com>
2020-01-03 14:55:35 +0700

    ver1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+double mathfunc(double x)
+{
+    double y = (3*pow(x,3)) + (2*2.71828) + pow(2,(2*x+1)) + sqrt(pow(x,2)+1);
+    return y;
+}
+int main()
+{
+    double x;
+    cout << "Enter x = ";
+    cin >> x;
+    cout << "Result y = "<<mathfunc(x);
+    return 0;
+
+}
