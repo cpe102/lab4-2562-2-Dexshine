0fe043b3ceaebf3d940b7127b09b625d9e7bad0c
Dexshine <b_o_y_s@hotmail.com>
2020-01-03 15:29:59 +0700

    ver1

--- lab4_4.cpp
+++ lab4_4.cpp
@@ -25,10 +25,12 @@ void printString2(string s1, string s2, int N){
 
 int main()
 {
+	string s,s1,s2;
 	int count = 0;
 	while(count < 10){
-		//Edit code here using printString1() and printString2()
-		
+		printString1("5",9-count);
+		printString2("6","9",count);
+		cout << endl;
 		count++;
 	}
     return 0;
