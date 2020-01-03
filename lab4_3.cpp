2690cda316b63f73f3adf53dac55e09613e5206b
Dexshine <b_o_y_s@hotmail.com>
2020-01-03 15:07:02 +0700

    ver1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+int findDivisor(int x)
+{
+    int i=x,y=0;
+    while(i>1)
+    {
+        if(x%i==0)
+        {
+            y = i;
+        }
+        i--;
+    }
+    return y;
+}
+int main()
+{
+    int x;
+    cin >> x;
+    cout << findDivisor(x);
+    return 0;
 
+}
