7699accb46701d6170200e612fa715e3cf3ab979
Dexshine <b_o_y_s@hotmail.com>
2020-01-03 14:55:48 +0700

    ver1

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
-//Write function findDistance() here
+double findDistance(double u,double a,double t)
+{
+  double s = (u*t) + (0.5*a*t*t);
+  return s;
+}
 
 int main(){
 
-  //Calling findDistance() using test cases
+  double u,a,t;
+  cin >> u;
+  cin >> a;
+  cin >> t;
+  cout << findDistance(u,a,t);
+
+
 
   return 0;
 }
